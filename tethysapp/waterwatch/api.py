970113a63dda8553b372b89f666e9cd5eb1ff47a
SarvaPulla <sarvapulla@gmail.com>
2018-07-25 02:52:04 -0500

    Fixed api

--- tethysapp/waterwatch/api.py
+++ tethysapp/waterwatch/api.py
@@ -22,23 +22,20 @@ def api_get_ponds(request):
 
 def api_get_timeseries(request):
     json_obj = {}
+
     if request.method == 'GET':
 
-        latitude = None
-        longitude = None
-
-        if request.GET.get('latitude'):
-            latitude = request.GET['latitude']
-        if request.GET.get('longitude'):
-            longitude = request.GET['longitude']
+        info = request.GET
+        lat = info.get('latitude')
+        lon = info.get('longitude')
 
         try:
-            ts_vals,coordinates = checkFeature(longitude,latitude)
+            ts_vals,coordinates,name = checkFeature(lon,lat)
             json_obj["values"] = ts_vals
             json_obj["coordinates"] = coordinates
+            json_obj["name"] = name
             json_obj["success"] = "success"
 
         except Exception as e:
             json_obj["error"] = "Error Processing Request. Error: "+ str(e)
-
     return JsonResponse(json_obj)
