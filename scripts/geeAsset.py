1c3c3272ee91918538aae2d90ecddce4c0be4a45
Billy Ashmall <billy.ashmall@nasa.gov>
2022-05-03 12:19:57 -0500

    enable daily automation

--- scripts/geeAsset.py
+++ scripts/geeAsset.py
@@ -3,6 +3,9 @@ import urllib.request
 import time
 from ee.batch import Export
 from datetime import date
+from datetime import datetime, timedelta
+import json
+
 ee.Initialize()
 
 
@@ -19,15 +22,16 @@ def landsatQaMask(img):
     # Both flags should be set to zero, indicating clear conditions.
     mask = (
         qa.bitwiseAnd(cloudShadowBitMask).eq(0)
-        .And(qa.bitwiseAnd(cloudsBitMask).eq(0))
+            .And(qa.bitwiseAnd(cloudsBitMask).eq(0))
     )
 
     # Return the masked image, scaled to reflectance, without the QA bands.
     return (
         img.updateMask(mask).divide(10000)
-        .select("B[0-9]*")
-        .copyProperties(img, ["system:time_start"])
+            .select("B[0-9]*")
+            .copyProperties(img, ["system:time_start"])
     )
+
 
 def sentinel2QaMask(img):
     """Custom QA masking method for Sentinel2 L1C/S2A dataset
@@ -41,9 +45,9 @@ def sentinel2QaMask(img):
 
     # Get s2cloudless image, subset the probability band.
     cld_prb = ee.Image(
-        S2_CLOUD_PROBA_COLL 
-        .filter(ee.Filter.eq("system:index", img.get("system:index")))
-        .first()
+        S2_CLOUD_PROBA_COLL
+            .filter(ee.Filter.eq("system:index", img.get("system:index")))
+            .first()
     ).select("probability")
 
     # Condition s2cloudless by the probability threshold value.
@@ -63,9 +67,9 @@ def sentinel2QaMask(img):
     # Project shadows from clouds for the distance specified by the CLD_PRJ_DIST input.
     cld_proj = (
         is_cloud.directionalDistanceTransform(shadow_azimuth, CLD_PRJ_DIST * 10)
-        .reproject(**{"crs": CRS, "scale": 120})
-        .select("distance")
-        .mask()
+            .reproject(**{"crs": CRS, "scale": 120})
+            .select("distance")
+            .mask()
     )
 
     # Identify the intersection of dark pixels with cloud shadow projection.
@@ -78,17 +82,18 @@ def sentinel2QaMask(img):
     # 20 m scale is for speed, and assumes clouds don't require 10 m precision.
     is_cld_shdw = (
         is_cld_shdw.focal_min(2)
-        .focal_max(BUFFER * 2 / 20)
-        .reproject(**{"crs": CRS, "scale": 60})
-        .rename("cloudmask")
+            .focal_max(BUFFER * 2 / 20)
+            .reproject(**{"crs": CRS, "scale": 60})
+            .rename("cloudmask")
     )
 
     # Subset reflectance bands and update their masks, return the result.
     return (
         img.updateMask(is_cld_shdw.Not())
-        .divide(10000).select("B.*")
-        .copyProperties(img, ["system:time_start"])
+            .divide(10000).select("B.*")
+            .copyProperties(img, ["system:time_start"])
     )
+
 
 def harmonize(s2img):
     """Function to apply and pass adjustment to S2 data
@@ -96,7 +101,7 @@ def harmonize(s2img):
     """
     landsatLike = s2img.multiply(GAIN).add(BIAS)
 
-    return landsatLike.copyProperties(s2img,["system:time_start"])
+    return landsatLike.copyProperties(s2img, ["system:time_start"])
 
 
 def mergeCollections(l8, s2, studyArea, t1, t2):
@@ -107,10 +112,10 @@ def mergeCollections(l8, s2, studyArea, t1, t2):
     # rename the bands we want to use
     lc8preprocess = (
         l8.filterBounds(studyArea)
-        .filterDate(t1, t2)
-        .filter(ee.Filter.lt('CLOUD_COVER', 75))
-        .map(landsatQaMask)
-        .select(
+            .filterDate(t1, t2)
+            .filter(ee.Filter.lt('CLOUD_COVER', 75))
+            .map(landsatQaMask)
+            .select(
             ['B2', 'B3', 'B4', 'B5', 'B6', 'B7'],
             ['blue', 'green', 'red', 'nir', 'swir1', 'swir2']
         )
@@ -119,20 +124,21 @@ def mergeCollections(l8, s2, studyArea, t1, t2):
     # preprocess the sentinel2 inputs
     # use same preprocessing workflow as landsat
     # except apply band pass adjustment at the end
-    st2preprocess= (
+    st2preprocess = (
         s2.filterBounds(studyArea)
-        .filterDate(t1, t2)
-        .filter(ee.Filter.lt('CLOUD_COVERAGE_ASSESSMENT', 75))
-        .map(sentinel2QaMask)
-        .select(
+            .filterDate(t1, t2)
+            .filter(ee.Filter.lt('CLOUD_COVERAGE_ASSESSMENT', 75))
+            .map(sentinel2QaMask)
+            .select(
             ['B2', 'B3', 'B4', 'B8', 'B11', 'B12'],
             ['blue', 'green', 'red', 'nir', 'swir1', 'swir2']
         )
-		.map(harmonize)
+            .map(harmonize)
     )
 
     return ee.ImageCollection(lc8preprocess.merge(st2preprocess))
-	
+
+
 def is_connected():
     """Function to make sure we are still connected to EE servers for
     long running processes
@@ -143,20 +149,22 @@ def is_connected():
     except urllib.request.URLError:
         return False
 
-def gond_waterclassifier(ndvi,ndwi,swir):
+
+def gond_waterclassifier(ndvi, ndwi, swir):
     """Implementation of surface water mapping algorithm from https://www.tandfonline.com/doi/abs/10.1080/0143116031000139908
     English description here: https://www.mdpi.com/1424-8220/20/2/431/
     """
 
     ndDiff = ndvi.subtract(ndwi)
-    ndDiffAvg = ndDiff.focal_mean(45,"square")
+    ndDiffAvg = ndDiff.focal_mean(45, "square")
     diffDiff = ndDiffAvg.subtract(ndDiff)
     indexWater = diffDiff.gte(0.08)
-    swirAvg = swir.focal_mean(45,"square")
+    swirAvg = swir.focal_mean(45, "square")
     swirDiff = swirAvg.subtract(swir)
     swirWater = swirDiff.gte(0.05)
 
     return indexWater.And(swirWater)
+
 
 def watermapping(img):
     """Function to calculate water indices and detect water using thresholds
@@ -164,10 +172,10 @@ def watermapping(img):
     """
     # calculate different indices to threshold
     # modified normalized difference water index
-    mndwi = img.normalizedDifference(["green","swir1"]).rename("mndwi")
+    mndwi = img.normalizedDifference(["green", "swir1"]).rename("mndwi")
 
     # normalized difference moisture index
-    ndmi = img.normalizedDifference(["nir","swir1"]).rename("ndmi")
+    ndmi = img.normalizedDifference(["nir", "swir1"]).rename("ndmi")
 
     # automated water extraction index (no shadow)
     aweinsh = img.expression(
@@ -229,9 +237,9 @@ def watermapping(img):
 
     # loop through the multiple indices classify water based on threshold values
     waters = []
-    for k,v in WATER_THRESHS.items():
+    for k, v in WATER_THRESHS.items():
         # check if we need to do < or > operator
-        if k in ["swir1","swir2"]:
+        if k in ["swir1", "swir2"]:
             water_id = indices.select(k).lt(ee.Number(v))
         else:
             water_id = indices.select(k).gt(ee.Number(v))
@@ -245,16 +253,52 @@ def watermapping(img):
     # gond_water = gond_waterclassifier(ndvi,ndwi, img.select("swir1")).rename("gond_water")
     # waters.append(gond_water)
 
-
     # concatenate all of the water images together and cast to byte datatype
     # valid values should be 0 (no water) or 1 (water)
-    out =  (
+    out = (
         ee.Image.cat(waters).uint8()
-        .copyProperties(img,["system:time_start"])
+            .copyProperties(img, ["system:time_start"])
     )
 
     return out
 
+
+def process_data(geometry, asset_id):
+    mergedCollection = mergeCollections(LC, S2, geometry, iniDate, endDate).sort('system:time_start', False)
+    # apply the multi-threshod water mapping process
+    processedCollection = mergedCollection.map(watermapping)
+    # get some info for exports
+    wqImages = processedCollection.size().getInfo()
+    wqicList = processedCollection.toList(wqImages)
+    print(f"Attemping to export {wqImages} images...")
+    # loop through the imagery to export
+    for i in range(wqImages):
+        if i < 0:
+            print("Skipping: " + str(i))
+        else:
+            try:
+                print("In export loop: " + str(i))
+                thisImg = ee.Image(wqicList.get(i))
+                name = thisImg.get('system:index').getInfo()
+                outScale = 30 if "LC08" in name else 10
+                print(f"Image name:{name}")
+                task = ee.batch.Export.image.toAsset(image=thisImg, description='ewf_ponds',
+                                                     assetId=asset_id + name,
+                                                     scale=outScale,
+                                                     maxPixels=1.0E13, region=thisImg.geometry())
+                while not is_connected:
+                    print("No connection: sleeping")
+                    time.sleep(1)
+                try:
+                    task.start()
+                except:
+                    while not is_connected():
+                        print("No connection: sleeping")
+                        time.sleep(1)
+                    task.start()
+            except Exception as e:
+                print(e)
+                continue
 
 
 # sentinel2 band pass adjustment coefficients
@@ -287,55 +331,20 @@ WATER_THRESHS = {
     "aewinsh": -1.3835,
 }
 
-# change dates to range to process
-iniDate = ee.Date('2021-05-28')
-today = ee.Date('2021-07-12')
-endDate = ee.Date(today)
+f = open('/home/tethys/apps/WaterWatch/scripts/region_info.json')
 
-# You will want to use ur features instead
-geometry = ee.Geometry.Polygon([[[-15.866,14.193],
-                                 [-12.990,14.193],
-                                 [-12.990,16.490],
-                                 [-15.866,16.490],
-                                 [-15.866,14.193]]])
+# returns JSON object as
+# a dictionary
+region_info = json.load(f)
+
+f.close()
+
+# change dates to range to process
+iniDate = ee.Date((datetime.today() - timedelta(days=1)).strftime('%Y-%m-%d'))  # 1 week prior
+
+endDate = ee.Date(ee.Date(datetime.today().strftime('%Y-%m-%d')))  # today
+
+for region in region_info["regions"]:
+    process_data(region['geometry'], region['gee_asset_id'])
 
 # preprocess and merge the landsat8 and sentinel2 data for a spatial/temporal domain
-mergedCollection = mergeCollections(LC, S2, geometry, iniDate, endDate).sort('system:time_start', False)
-
-# apply the multi-threshod water mapping process
-processedCollection =  mergedCollection.map(watermapping)
-
-# get some info for exports
-wqImages = processedCollection.size().getInfo()
-wqicList = processedCollection.toList(wqImages)
-
-print(f"Attemping to export {wqImages} images...")
-
-# loop through the imagery to export
-for i in range(wqImages):
-    if i < 0:
-        print("Skipping: " + str(i))
-    else:
-        try:
-            print("In export loop: " + str(i))
-            thisImg = ee.Image(wqicList.get(i))
-            name = thisImg.get('system:index').getInfo()
-            outScale = 30 if "LC08" in name else 10
-            print(f"Image name:{name}")
-            task = ee.batch.Export.image.toAsset(image= thisImg, description='ewf_ponds',
-                                                 assetId='projects/servir-wa/services/ephemeral_water_ferlo/processed_ponds/' + name, scale=outScale,
-                                                 maxPixels=1.0E13, region=thisImg.geometry())
-            while not is_connected:
-                print("No connection: sleeping")
-                time.sleep(1)
-            try:
-                task.start()
-            except:
-                while not is_connected():
-                    print("No connection: sleeping")
-                    time.sleep(1)
-                task.start()
-        except Exception as e:
-            print(e)
-            continue
-
